f9d398004cd3b3f9ed9341e4f6be151e5be007b3
Maxwell Rutherford <javert.hugo@yandex.ru>
2022-07-19 22:21:46 +0300

    init project

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import path from 'path'
+import template from './../template'
+import mongoose from 'mongoose'
+//comment out before building for production
+import devBundle from './devBundle'
+
+
+const app = express()
+//comment out before building for production
+devBundle.compile(app)
+
+const CURRENT_WORKING_DIR = process.cwd()
+app.use('dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+app.get('/', (req, res) => {
+    res.status(200).send(template())
+})
+
+let port = process.env.PORT || 3000
+
+app.listen(port, err => {
+    if (err) {
+        console.log(err)
+    }
+    console.info(`Server started on port ${port}`)
+})
+
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/test'
+mongoose.connect(url, (err, db) => {
+    console.log('Connected successfully to mongodb server')
+    db.close()
+})
