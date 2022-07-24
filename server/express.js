45bac33dd76a7375c0bb95b7e10519e3aa452f7f
Maxwell Rutherford <javert.hugo@yandex.ru>
2022-07-24 23:32:00 +0300

    add auth controller

--- server/express.js
+++ server/express.js
@@ -6,6 +6,7 @@ import cors from 'cors'
 import helmet from 'helmet'
 import Template from '../template'
 import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
 
 const app = express()
 
@@ -16,6 +17,23 @@ app.use(compress())
 app.use(helmet())
 app.use(cors())
 
-app.use('/', userRoutes)    
+app.use('/', userRoutes)
+app.use('/', authRoutes)
+
+// We need the following code to handle the authorization errors that express-jwt
+// produces when trying to verify JWT tokens in incoming requests,
+// express-jwt throws an error named UnauthorizedError when a token cannot be
+// validated for some reason. We catch this error here to return a 401 status back to the
+// requesting client. We also add a response to be sent if other server-side errors are
+// generated and caught here.
+
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ 'error': `${err.name}: ${err.message}`  })
+    } else if (err) {
+        res.status(400).json({ 'error': `${err.name}: ${err.message}`  })
+        console.log(err)
+    }
+})
 
 export default app
