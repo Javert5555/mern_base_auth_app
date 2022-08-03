f84d30ef8e016a5009109d188d557b203bec346f
Maxwell Rutherford <javert.hugo@yandex.ru>
2022-08-03 17:28:06 +0300

    fix webpack.prod.config

--- webpack.config.client.production.js
+++ webpack.config.client.production.js
@@ -15,7 +15,7 @@ const config = {
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.js$|jsx/,
                 exclude: /node_modules/,
                 use: [
                     'babel-loader'
